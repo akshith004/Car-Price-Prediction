c93b7bdbeb110b490f608809e27dd97c6eae060b
Akshith Kumar Biyyala <164652643+akshith004@users.noreply.github.com>
2025-11-02 10:28:26 +0530

    Delete practice.py

--- practice.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Hello How are you")
-st.write("What are you doing right there")
-name = st.text_input("Enter your name")
-if st.button("namaste"):
-    st.success(f'Hello {name}')
