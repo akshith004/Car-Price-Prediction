1ad1c74d69c00232a0788c1ee990245747fe6e70
akshith004 <akshithbiyyala@gmail.com>
2025-11-02 10:36:59 +0530

    Minor edits

--- app.py
+++ app.py
@@ -12,39 +12,20 @@ st.write("Enter details to predict the car's selling price")
 
 # User inputs
 car_models = [
-    # BMW
     "BMW X5", "BMW 3 Series", "BMW X3", "BMW 5 Series", "BMW X1", "BMW Z4",
-    
-    # Audi
     "Audi A5", "Audi Q5", "Audi Q7", "Audi A6", "Audi A4", "Audi TT",
-    
-    # Mercedes-Benz
     "Mercedes Benz C class", "Mercedes Benz E class", "Mercedes Benz GLC",
     "Mercedes Benz S class", "Mercedes Benz CLA", "Mercedes Benz GLE",
-    
-    # Toyota
     "Toyota Corolla", "Toyota Camry", "Toyota RAV4", "Toyota Highlander", "Toyota Prius",
-    
-    # Honda
     "Honda Civic", "Honda Accord", "Honda CR-V", "Honda Pilot", "Honda Fit",
-    
-    # Ford
     "Ford Mustang", "Ford F-150", "Ford Explorer", "Ford Edge", "Ford Focus",
-    
-    # Tesla
     "Tesla Model 3", "Tesla Model S", "Tesla Model X", "Tesla Model Y", "Tesla Roadster", "Tesla Cybertruck",
-    
-    # Hyundai
     "Hyundai Tucson", "Hyundai Elantra", "Hyundai Santa Fe", "Hyundai Kona", "Hyundai Ioniq",
-    
-    # Kia
     "Kia Seltos", "Kia Sportage", "Kia Soul", "Kia Stinger", "Kia Carnival",
-    
-    # Nissan
     "Nissan Altima", "Nissan Sentra", "Nissan Maxima", "Nissan Rogue", "Nissan Murano"
 ]
 car_model = st.selectbox("Car Model", car_models)
-mileage = st.number_input("Mileage (in kms)", min_value=0, value=0, step=1000)
+mileage = st.number_input("KM Travelled", min_value=0, value=0, step=1000)
 age = st.number_input("Age of the car (in years)", min_value=0, value=0, step=1)
 
 # Convert user input to model format
